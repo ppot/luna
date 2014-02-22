4e341a4be78c06bcfa6b351ba756cc9dc9b07375
William Gomez <Wily@Williams-MacBook-Pro-2.local>
2014-02-22 17:03:53 -0500

    la base de donnée complète mais sans contenu, admins_connexion.html a été ajouté et ajout des input pour l'adresse dans index.html

--- public/js/app.main.js
+++ public/js/app.main.js
@@ -1,16 +1,23 @@
 function app_module_log() {
-  el = document.getElementById("overlay");
-  el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
+  	el = document.getElementById("overlay");
+  	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 	join();
 }
 
-function signin(){
+function signin() {
 	$('#signin').show();
 	$('#register').hide();
 	$('.already-member').hide();
 }
-function join(){
+function join() {
 	$('#signin').hide();
 	$('#register').show();
 	$('.already-member').show();
 }
+
+function admins_log() {
+	user = $("#admin_utilisateur").val()
+	pass = $('#admin_mot_de_passe').val();
+	alert(user + " : " + pass);
+}
+
